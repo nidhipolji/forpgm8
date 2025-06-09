d8a105cb8d88f74ee4bb5e57df5d56f550bfbd8a
nidhipolji <nidhipolji24@gmail.com>
2025-06-09 20:06:51 +0530

    Add files via upload

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=10,b=20;
+        int sum=a+b;
+        System.out.println("Sum of "+a+" + "+b+" is : "+ sum);
+    }
+}
